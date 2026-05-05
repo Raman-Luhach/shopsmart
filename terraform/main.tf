94170d204a4d33c6a33d0118e1f956ea44d0b072
Raman-Luhach <ramanluhach369@gmail.com>
2026-05-05 23:12:08 +0530

    fix 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket = "shopsmart-artifacts-rl-2026"
-    key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
